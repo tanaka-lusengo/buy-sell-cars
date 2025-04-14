f04dcfc84d5ccef7b1d845067f63ad94722dc08d
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2025-04-14 23:18:16 +0100

    feat: add `@pandacss/dev`

---
+++ panda.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+  include: [
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  exclude: [],
+  theme: {
+    extend: {},
+  },
+  presets: ['@pandacss/preset-base'],
+  jsxFramework: 'react',
+  outdir: 'styled-system',
+});
---
+++ src/app/page.styled.ts
@@ -0,0 +1,9 @@
+import { styled } from '@/styled-system/jsx';
+
+export const Typography = styled('h1', {
+  base: {
+    fontSize: 'x-large',
+    fontWeight: 'bold',
+    color: 'darkseagreen',
+  },
+});
