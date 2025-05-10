bc047c3503cc46ce432897aa3fb4343e9bade427
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2025-05-10 18:53:07 +0200

    feat: add supabase browser and server client, add middleware

---
+++ supabase/client.ts
@@ -0,0 +1,9 @@
+import { createBrowserClient } from '@supabase/ssr';
+import { Database } from '@/database.types';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+export function createClient() {
+  return createBrowserClient<Database>(supabaseUrl, supabaseAnonKey);
+}
