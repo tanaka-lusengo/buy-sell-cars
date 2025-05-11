082cb98f2fbd9ff6b4615c3a3baf99320fb8f748
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2025-05-11 08:59:48 +0200

    feat: move `values` constant to shared, add helper util functions:
    - `generateYears()`
    - `generatePrices()`

---
+++ src/constants/values.ts
@@ -0,0 +1,18 @@
+export const CAR_CONDITIONS = ['New', 'Used'];
+
+export const FUEL_TYPES = ['Petrol', 'Diesel', 'Electric', 'Hybrid'];
+
+export const GEARBOX_TYPES = ['Manual', 'Automatic'];
+
+export const VEHICLE_CATEGORIES = [
+  'Car rental',
+  'New car',
+  'Used car',
+  'Boat',
+  'Motorcycle',
+  'Truck',
+  'Agriculture',
+  'Earth moving',
+];
+
+export const LISTING_TYPES = ['Rental', 'For Sale'];
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export * from './constants';
 export * from './reactHotToast';
 export * from './errorHandlers';
+export * from './generateData';
+export { toSnakeCase } from './toSnakeCase';
