ac8fd4830a3b9118c06b58d5b2127051faf61f7f
tanaka lusengo <tanaka.lusengo@kraken.tech>
2025-07-02 11:28:44 +0100

    feat(shared-ui): add FAQAccordion

---
+++ packages/shared/ui/FAQAccordion/FAQAccordion.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useState } from "react";
+import { css } from "../../styled-system/css";
+import { Flex, Box } from "../../styled-system/jsx";
+import { generateIcon } from "../../utils";
+import { Typography } from "../Typography/Typography";
+
+type FAQAccordionProps = {
+  title: string;
+  content: React.ReactElement;
+};
+
+export const FAQAccordion = ({ title, content }: FAQAccordionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const buttonStyles = css({
+    width: "100%",
+    paddingY: "md",
+    paddingX: "lg",
+    border: "none",
+    cursor: "pointer",
+    textAlign: "left",
+    fontWeight: "medium",
+    fontSize: "md",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    transition: "background-color 0.3s ease",
+    _hover: { bg: "greyLight" },
+    _focus: {
+      outline: "2px solid",
+      outlineColor: "secondary",
+      outlineOffset: "2px",
+      backgroundColor: "greyLight",
+    },
+  });
+
+  return (
+    <Box
+      borderWidth="1px"
+      borderColor="grey"
+      borderRadius="1.2rem"
+      overflow="hidden"
+      bg="white"
+      shadow="md"
+    >
+      <button onClick={toggleAccordion} className={buttonStyles}>
+        <Typography as="span" weight="bold">
+          {title}
+        </Typography>
+        <Box
+          transform={isOpen ? "rotate(180deg)" : "rotate(0deg)"}
+          transition="transform 0.3s ease"
+          fontSize="lg"
+        >
+          {generateIcon("chevron-down")}
+        </Box>
+      </button>
+
+      <Box
+        overflow="hidden"
+        transition="all 0.3s ease"
+        maxHeight={isOpen ? "1500px" : "0"}
+        opacity={isOpen ? 1 : 0}
+      >
+        <Box paddingY="md" paddingX="lg" borderTopColor="secondary">
+          <Flex direction="column" gap="md" minHeight="fit-content">
+            {content}
+          </Flex>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
