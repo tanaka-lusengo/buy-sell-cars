a7846608dad18e015cc8a41d4e4be0a9ba4f03df
tanaka lusengo <tanaka.lusengo@kraken.tech>
2025-10-24 17:50:45 +0100

    chore: update urls

--- apps/buy-sell-cars-zimbabwe/src/constants/sponsors.ts
+++ apps/buy-sell-cars-zimbabwe/src/constants/sponsors.ts
@@ -3,4 +3,18 @@ export const SPONSOR_NAMES = {
   ROAD_BOYS_LOGISTICS: "Road Boys Logistics",
   REFUEL: "Refuel",
   SUPA_CAR_SOUNDS: "Supa Car Sounds",
+  SPRAY_TECH: "Spray Tech",
+  CERAMIC_PRO: "Ceramic Pro",
 } as const;
+
+export const SPONSOR_URL = {
+  ROSSI_TYRES_URL:
+    "https://www.facebook.com/rossihararedrive?mibextid=wwXIfr&rdid=XD0b641mH0N7Z55o&share_url=https://www.facebook.com/share/1BSNJucW2A/?mibextid%3DwwXIfr",
+  ROAD_BOYS_LOGISTICS_URL:
+    "https://www.facebook.com/profile.php?id=61559339190662&mibextid=wwXIfr&rdid=hT2V2GBBlE0TmbTJ&share_url=https://www.facebook.com/share/1BvWhxyunK/?mibextid%3DwwXIfr",
+  REFUEL_WHATSAPP_URL: "https://wa.me/+263775158370",
+  SUPA_CAR_SOUNDS_URL: "https://www.facebook.com/supasounds.co.zw/about/",
+  SPRAY_TECH_URL: "https://spraytechzw.com",
+  CERAMIC_PRO_URL:
+    "https://www.instagram.com/ceramicprozimbabwe?igsh=bWk0d2V1Z2tzbWlj",
+};
